f083e5ef375821bebe7c94978f11822119680794
ahmed <ahmedbarakat607080@gmail.com>
2023-07-05 16:07:43 +0300

    Projects

---
+++ EO elements in array/EO elements in array/EO elements in array/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EO_elements_in_array
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter size");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("array element :");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            findevenodd(arr);
+        }
+        public static void findevenodd(int[] arr)
+        {
+            int evencount = 0, oddcount = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] % 2 == 0)
+                {
+                    Console.WriteLine("{0} is even number ", arr[i]);
+                    evencount++;
+                }
+                else
+                {
+                    Console.WriteLine("{0} is odd number ", arr[i]);
+                    oddcount++;
+                }
+            }
+            Console.WriteLine("The number of even number ={0}", evencount);
+            Console.WriteLine("The number of odd number ={0}", oddcount);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Research in array (2)/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the array size");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int [size];
+            Console.WriteLine("Enter the array values");
+            for (int i = 0; i < arr.Length; i++)
+            { 
+             arr[i]= int.Parse(Console.ReadLine());           
+            }
+
+            int J = -1;
+            Console.WriteLine("Enter the value to search it in array or not ");
+            int X = int.Parse(Console.ReadLine());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (X == arr[i])
+                {
+                    J = i;
+                    break;
+                }
+            }
+            if (J == -1)
+                Console.WriteLine(" The element not found");
+            else
+                Console.WriteLine(" The element is found at index " + J);
+
+            Console.ReadLine();
+        }
+
+    }
+}
